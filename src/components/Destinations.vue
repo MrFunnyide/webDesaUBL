23ea9abf8344fa6a6295596b37cd8261eb94613a
MrFunnyide <12050113503@students.uin-suska.ac.id>
2023-08-21 08:34:33 +0700

    change image

--- src/components/Destinations.vue
+++ src/components/Destinations.vue
@@ -11,7 +11,7 @@
       <div class="d-flex justify-content-center ">
          <div class="row w-50 mt-5">
             <div class="col-lg-6 col-md-12 mb-4 mb-lg-0">
-               <img src="../assets/image/curup4.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+               <img src="../assets/image/curup1.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
 
                <img src="../assets/image/curup2.jpg" class="w-100 shadow-1-strong rounded mb-4"
                   alt="Wintry Mountain Landscape" />
@@ -20,8 +20,8 @@
             <div class="col-lg-6 mb-4 mb-lg-0">
                <img src="../assets/image/curup3.jpg" class="w-100 shadow-1-strong rounded mb-4"
                   alt="Mountains in the Clouds" />
-               <img src="../assets/image/curup1.JPG" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
                <img src="../assets/image/curup5.JPG" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+               <!-- <img src="../assets/image/curup5.JPG" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" /> -->
             </div>
          </div>
       </div>
